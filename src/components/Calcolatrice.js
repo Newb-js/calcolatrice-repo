bdaa3943b9d205b48d48f401befc9e639133314d
Paz <85231543+Newb-js@users.noreply.github.com>
2021-06-08 17:41:19 +0200

    Rimosso Pulsante.js

--- src/components/Calcolatrice.js
+++ src/components/Calcolatrice.js
@@ -3,12 +3,35 @@ import Display from "./Display";
 import Tastiera from "./Tastiera";
 
 function Calcolatrice() {
-  const [data, setData] = useState("pene");
+  const [data, setData] = useState("");
+
+  calcolo = () => {
+    try {
+      const risultato = eval(data);
+      setData(risultato);
+    } catch (e) {
+      setData("errore");
+    }
+  };
+
+  gestoreClick = (e) => {
+    const value = e.target.getAttribute("data-value");
+    switch (value) {
+      case "clear":
+        setData("");
+        break;
+      case "equal":
+        calcolo();
+        break;
+      default:
+        setData(data + value);
+    }
+  };
 
   return (
     <div className="calcolatrice">
       <Display />
-      <Tastiera dataDrill={data} />
+      <Tastiera data={data} />
     </div>
   );
 }
--- src/components/Tastiera.js
+++ src/components/Tastiera.js
@@ -1,31 +1,40 @@
 import React from "react";
-import Pulsante from "./Pulsante";
 
 const Tastiera = (props) => {
   return (
     <div className="tastiera">
-      <Pulsante label={props.dataDrill} value="clear" />
-      <Pulsante label="7" value="7" />
-      <Pulsante label="4" value="4" />
-      <Pulsante label="1" value="1" />
-      <Pulsante label="0" value="0" />
+      <Pulsante label={props.dataDrill} valorePulsante="clear" />
+      <Pulsante label="7" valorePulsante="7" />
+      <Pulsante label="4" valorePulsante="4" />
+      <Pulsante label="1" valorePulsante="1" />
+      <Pulsante label="0" valorePulsante="0" />
 
-      <Pulsante label="/" value="/" />
-      <Pulsante label="8" value="8" />
-      <Pulsante label="5" value="5" />
-      <Pulsante label="2" value="2" />
-      <Pulsante label="." value="." />
+      <Pulsante label="/" valorePulsante="/" />
+      <Pulsante label="8" valorePulsante="8" />
+      <Pulsante label="5" valorePulsante="5" />
+      <Pulsante label="2" valorePulsante="2" />
+      <Pulsante label="." valorePulsante="." />
 
-      <Pulsante label="x" value="x" />
-      <Pulsante label="9" value="9" />
-      <Pulsante label="6" value="6" />
-      <Pulsante label="3" value="3" />
-      <Pulsante label="" value="null" />
+      <Pulsante label="x" valorePulsante="x" />
+      <Pulsante label="9" valorePulsante="9" />
+      <Pulsante label="6" valorePulsante="6" />
+      <Pulsante label="3" valorePulsante="3" />
+      <Pulsante label="" valorePulsante="null" />
 
-      <Pulsante label="-" value="-" />
-      <Pulsante label="+" value="+" />
-      <Pulsante label="=" value="=" />
+      <Pulsante label="-" valorePulsante="-" />
+      <Pulsante label="+" valorePulsante="+" />
+      <Pulsante label="=" valorePulsante="=" />
     </div>
+  );
+};
+
+const Pulsante = (props) => {
+  return (
+    <>
+      <button className="pulsante" value={props.valorePulsante}>
+        {props.label}
+      </button>
+    </>
   );
 };
 
